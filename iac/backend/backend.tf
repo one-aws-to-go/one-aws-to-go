d72072d5dfe920872af824e96622fc72a4509ab9
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2022-09-19 15:41:06 +0300

    Action triggers

--- iac/backend/backend.tf
+++ iac/backend/backend.tf
@@ -1,12 +1,7 @@
 variable "APP_NAME" {}
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
-variable "AWS_REGION" {}
 
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region = var.AWS_REGION
+  # Setup in env
 }
 
 resource "aws_s3_bucket" "tf_state" {
