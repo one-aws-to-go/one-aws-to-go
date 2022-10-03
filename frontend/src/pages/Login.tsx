bbb3b0737c6028170649dc06b720663b10edfadd
Veikko Hiltunen <veikko.hiltun@gmail.com>
2022-10-03 15:55:29 +0300

    Modify borders from login page input field

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,29 +1,36 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
   let navigate = useNavigate();
 
+  const submitForm = () => {
+    navigate('/main')
+  }
+
   return (
     <div className='h-screen bg-office bg-cover'>
       <div className='flex flex-col h-screen backdrop-brightness-15 justify-center items-center'>
-        <p className='text-5xl text-center text-primary font-bold'>Awtq</p>
-        <p className='text-base text-center text-white font-bold'>
+        <p className='text-6xl text-center text-primary font-bold'>Awtq</p>
+        <p className='text-base text-center text-white font-semibold'>
           "One AWS To Go"
         </p>
         <div>
           <div className='flex flex-row w-full justify-center'>
             <div className='flex flex-col justify-start items-start space-y-1'>
-              <div className='flex flex-row w-56 items-center justify-center mt-2'>
+              <form
+                className='flex flex-row w-56 items-center justify-center mt-2'
+                onSubmit={submitForm}
+              >
                 <input
-                  className='bg-black bg-opacity-50 shadow appearance-none w-full px-3 py-2 h-full text-white focus:outline-none focus:shadow-outline placeholder:align-top leading-none'
-                  id='username'
+                  className='bg-black bg-opacity-50 shadow rounded-l border-2 appearance-none w-full px-3 py-2 h-full text-white focus:outline-none focus:shadow-outline placeholder:align-bottom'
+                  id='token'
                   type='text'
                   placeholder='Token'
                 />
                 <button
-                  onClick={() => navigate('/main')}
-                  type='button'
-                  className='text-white bg-primary focus:outline-none font-medium text-sm px-5 h-full  text-center inline-flex items-center dark:bg-primary dark:hover:bg-orange-400'
+                  type='submit'
+                  className='text-white bg-primary rounded-r focus:outline-none font-medium text-sm px-5 h-full  text-center inline-flex items-center dark:bg-primary dark:hover:bg-orange-400'
                 >
                   <svg
                     width='21'
@@ -39,14 +46,14 @@ const Login = () => {
                     />
                   </svg>
                 </button>
-              </div>
+              </form>
               <a
                 className='text-center text-primary text-xs'
                 target='_blank'
                 href='https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
                 rel='noreferrer'
               >
-                Create new token here
+                Create a new token here
               </a>
             </div>
           </div>
