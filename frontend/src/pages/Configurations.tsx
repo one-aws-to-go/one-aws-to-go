e7d5e502930ec80f02c3c88218cb5212026fb5b7
Justus Kajava <justuskajava@gmail.com>
2022-10-03 14:47:49 +0300

    Add menu to Configurations

--- frontend/src/pages/Configurations.tsx
+++ frontend/src/pages/Configurations.tsx
@@ -1,35 +1,43 @@
+import Menu from "../components/Menu";
 
 const Configurations = () => {
-    return (
-      <div
-        className='space-y-4 bg-black flex flex-col h-screen justify-center items-center'
-        >
-
-    
-        <p className='text-2xl text-center text-primary font-bold'>Configuration</p>  
-        <input 
-        className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" 
-        id="project name" 
-        type="text" 
-        placeholder="Project name"></input>
-        <input 
-        className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" 
-        id="aws secret" 
-        type="text" 
-        placeholder="AWS Secret"></input>
-        <input 
-        className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" 
-        id="aws key" 
-        type="text" 
-        placeholder="AWS Key"></input>
-        
-        <button className="rounded-lg bg-transparent text-white font-semibold py-2 px-4 border border-primary">
-        Button
-        </button>
-
-        
+  return (
+    <div className="bg-black h-screen">
+      <div className="pt-10 pb-10">
+        <Menu />
       </div>
-    );
-  };
+      <form action="#" method="post">
+        <div className="gap-4  flex flex-col justify-center items-center">
+          <p className="text-3xl text-center text-primary font-bold pb-20">
+            Configuration
+          </p>
+          <p className="text-white">Enter the details:</p>
+          <input
+            className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            id="project name"
+            type="text"
+            placeholder="Project name"
+          ></input>
+          <input
+            className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            id="aws key"
+            type="text"
+            placeholder="AWS Key"
+          ></input>
+          <input
+            className="bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+            id="aws secret"
+            type="text"
+            placeholder="AWS Secret"
+          ></input>
 
-  export default Configurations;
+          <button className="rounded-lg bg-transparent text-white font-semibold py-2 px-4 border border-primary hover:border-primary-variant">
+            To the moon 🚀
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Configurations;
