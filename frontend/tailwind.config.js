cc67456e7574a0d1bdc6c73fdb01427575a96229
Veikko Hiltunen <veikko.hiltun@gmail.com>
2022-10-03 11:54:23 +0300

    Fix colors in tailwind configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,20 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      white: "#ffffff",
-      divider: "#c2c2c2",
-      primary: "#f39c12",
-      "primary-container": "#121111",
-      "primary-variant": "#492f05",
-      surface: "#0d0d0d",
-      success: "#388E3C",
-      error: "#b00d20",
-    },
     fontFamily: {
       sans: ["Khula", "Graphik", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      brightness: {
+        15: '.15'
+      },
+      backgroundImage: {
+        'office': "url('/img/background_image.jpg')",
+      },
+      colors: {
+        white: "#ffffff",
+        divider: "#c2c2c2",
+        primary: "#f39c12",
+        "primary-container": "#121111",
+        "primary-variant": "#492f05",
+        surface: "#0d0d0d",
+        success: "#388E3C",
+        error: "#b00d20",
+      },
+    },
   },
   plugins: [],
 };
