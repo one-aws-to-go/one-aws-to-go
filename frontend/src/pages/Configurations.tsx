add38fe854219943240186e8b7a969ac1e1e1a93
Veikko Hiltunen <veikko.hiltun@gmail.com>
2022-10-04 10:58:02 +0300

    Modify configuration window to center content properly

--- frontend/src/pages/Configurations.tsx
+++ frontend/src/pages/Configurations.tsx
@@ -2,37 +2,39 @@ import NavBar from '../components/NavBar';
 
 const Configurations = () => {
   return (
-    <div className='bg-black h-screen'>
+    <div className='bg-black h-screen flex flex-col'>
       <NavBar />
-      <form action='#' method='post'>
-        <div className='gap-4  flex flex-col justify-center items-center'>
-          <p className='text-3xl text-center text-primary font-bold pb-20'>
-            Configuration
-          </p>
-          <p className='text-white'>Enter the details:</p>
-          <input
-            className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
-            id='project name'
-            type='text'
-            placeholder='Project name'
-          ></input>
-          <input
-            className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
-            id='aws key'
-            type='text'
-            placeholder='AWS Key'
-          ></input>
-          <input
-            className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
-            id='aws secret'
-            type='text'
-            placeholder='AWS Secret'
-          ></input>
+      <form
+        action='#'
+        method='post'
+        className='gap-4  flex flex-1 flex-col justify-center items-center'
+      >
+        <p className='text-3xl text-center text-primary font-bold'>
+          Configuration
+        </p>
+        <p className='text-white'>Enter the details:</p>
+        <input
+          className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
+          id='project name'
+          type='text'
+          placeholder='Project name'
+        ></input>
+        <input
+          className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
+          id='aws key'
+          type='text'
+          placeholder='AWS Key'
+        ></input>
+        <input
+          className='bg-black shadow appearance-none border rounded py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
+          id='aws secret'
+          type='text'
+          placeholder='AWS Secret'
+        ></input>
 
-          <button className='rounded-lg bg-transparent text-white font-semibold py-2 px-4 border border-primary hover:border-primary-variant'>
-            To the moon 🚀
-          </button>
-        </div>
+        <button className='rounded-lg bg-transparent text-white font-semibold py-2 px-4 border border-primary hover:border-primary-variant'>
+          To the moon 🚀
+        </button>
       </form>
     </div>
   );
