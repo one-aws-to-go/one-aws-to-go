9253378b29d78e952657b29f9268a68596b5c882
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2022-10-24 10:05:29 +0300

    Create forks page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,14 @@
-//import "./Typography.css";
-
 import './index.css';
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from './App';
-import Configurations from './pages/Configurations';
+import Forks from './pages/Forks';
 import { CookiesProvider } from 'react-cookie';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Logs from './pages/Logs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Test from './Test';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -33,8 +29,8 @@ const router = createBrowserRouter([
     element: <Logs />,
   },
   {
-    path: 'configurations',
-    element: <Configurations />,
+    path: 'forks',
+    element: <Forks />,
   },
 ]);
 
