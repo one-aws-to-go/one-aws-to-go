67b8a8c49cea0e8c5e61441399af7bee93a91e94
Veikko Hiltunen <veikko.hiltun@gmail.com>
2022-10-25 21:20:34 +0300

    Remove unused files

--- frontend/src/service.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import { BACKEND_URL } from './config'
-import { BackendResponse } from './model'
-
-export const getMessage = async (testValue?: string): Promise<BackendResponse> => {
-  const response = await axios.get(`${BACKEND_URL}?test=${testValue || 'Frontend'}`)
-  return response.data
-}
