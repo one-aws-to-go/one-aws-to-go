9a5f77453725ddf595c199b821e7149c87ddf572
Iiro Kiviluoma <56997609+iiroki@users.noreply.github.com>
2022-11-09 14:18:11 +0200

    OATG-63: Cloud DB migrations (#20)

--- iac/solution/modules/rds/output.tf
+++ iac/solution/modules/rds/output.tf
@@ -13,3 +13,7 @@ output "db_endpoint" {
 output "db_name" {
   value = aws_db_instance.db.db_name
 }
+
+output "pg_db_url" {
+  value = "postgresql://${aws_db_instance.db.username}:${aws_db_instance.db.password}@${aws_db_instance.db.endpoint}/${aws_db_instance.db.db_name}"
+}
