ed321cf636ccc254dba803e4946a1799f2f515c9
Jussi Kujanen <jussi.kujanen@vincit.fi>
2022-11-14 15:03:49 +0200

    OATG-54 Add actions tests

--- backend/test/fork.spec.ts
+++ backend/test/fork.spec.ts
@@ -22,97 +22,95 @@ describe('Forks handlers', () => {
   beforeEach(async () => {
     await resetDb()
   })
-  describe('Post', () => {
-    describe('Happy', () => {
-      it('Should be able to create a fork', async () => {
-        await createMockUser()
-        mockedAxios.get.mockResolvedValue({
-          data: getMockGithubUserFields()
-        })
+  describe('Happy', () => {
+    it('Should be able to create a fork', async () => {
+      await createMockUser()
+      mockedAxios.get.mockResolvedValue({
+        data: getMockGithubUserFields()
+      })
 
-        const payload: AuthorizedEvent = getMockAuthorizedEvent({
-          name: 'test',
-          templateId: 1
-        })
-        const result = await postForkHandler(payload)
-        const body = JSON.parse(result.body)
-        expect(result.statusCode).toEqual(HttpStatus.CREATED)
-        expect(body.appName).toEqual('test')
-        expect(body.state).toEqual(ForkState.CREATED)
+      const payload: AuthorizedEvent = getMockAuthorizedEvent({
+        name: 'test',
+        templateId: 1
       })
-      it('Should create user when creating fork', async () => {
-        mockedAxios.get.mockResolvedValue({
-          data: getMockGithubUserFields()
-        })
-        const payload: AuthorizedEvent = getMockAuthorizedEvent({
-          name: 'test',
-          templateId: 1
-        })
-        expect(await prisma.user.findFirst()).toBe(null)
-        await postForkHandler(payload)
-        const userInDb = await prisma.user.findFirst({})
-        expect(userInDb).not.toBe(null)
+      const result = await postForkHandler(payload)
+      const body = JSON.parse(result.body)
+      expect(result.statusCode).toEqual(HttpStatus.CREATED)
+      expect(body.appName).toEqual('test')
+      expect(body.state).toEqual(ForkState.CREATED)
+    })
+    it('Should create user when creating fork', async () => {
+      mockedAxios.get.mockResolvedValue({
+        data: getMockGithubUserFields()
       })
-      it('Should be able to get created fork', async () => {
-        const createdUser = await createMockUser()
-        const createdFork = await createMockFork(createdUser.githubId)
-        mockedAxios.get.mockResolvedValue({
-          data: getMockGithubUserFields()
-        })
-        const payload = getMockAuthorizedEvent({}, { id: createdFork.id })
+      const payload: AuthorizedEvent = getMockAuthorizedEvent({
+        name: 'test',
+        templateId: 1
+      })
+      expect(await prisma.user.findFirst()).toBe(null)
+      await postForkHandler(payload)
+      const userInDb = await prisma.user.findFirst({})
+      expect(userInDb).not.toBe(null)
+    })
+    it('Should be able to get created fork', async () => {
+      const createdUser = await createMockUser()
+      const createdFork = await createMockFork(createdUser.githubId)
+      mockedAxios.get.mockResolvedValue({
+        data: getMockGithubUserFields()
+      })
+      const payload = getMockAuthorizedEvent({}, { id: createdFork.id })
 
-        const result = await getForkHandler(payload)
-        const body = JSON.parse(result.body)
-        expect(result.statusCode).toEqual(HttpStatus.OK)
-        expect(body.appName).toEqual('test')
+      const result = await getForkHandler(payload)
+      const body = JSON.parse(result.body)
+      expect(result.statusCode).toEqual(HttpStatus.OK)
+      expect(body.appName).toEqual('test')
+    })
+    it('Should be able to get all created forks', async () => {
+      const createdUser = await createMockUser()
+      await createMockFork(createdUser.githubId)
+      await createMockFork(createdUser.githubId, 1, {
+        appName: 'test2',
+        owner: 'test2'
       })
-      it('Should be able to get all created forks', async () => {
-        const createdUser = await createMockUser()
-        await createMockFork(createdUser.githubId)
-        await createMockFork(createdUser.githubId, 1, {
-          appName: 'test2',
-          owner: 'test2'
-        })
-        mockedAxios.get.mockResolvedValue({
-          data: getMockGithubUserFields()
-        })
-        const payload = getMockAuthorizedEvent()
+      mockedAxios.get.mockResolvedValue({
+        data: getMockGithubUserFields()
+      })
+      const payload = getMockAuthorizedEvent()
 
-        const result = await getForksHandler(payload)
-        const body = JSON.parse(result.body)
-        expect(result.statusCode).toEqual(HttpStatus.OK)
-        expect(body.length).toEqual(2)
+      const result = await getForksHandler(payload)
+      const body = JSON.parse(result.body)
+      expect(result.statusCode).toEqual(HttpStatus.OK)
+      expect(body.length).toEqual(2)
+    })
+    it('Should be able to set secrets for fork', async () => {
+      const createdUser = await createMockUser()
+      const createdFork = await createMockFork(createdUser.githubId)
+      const secretsPayload: ForkAwsSecretArgs = {
+        awsAccessKey: 'ASD',
+        awsDefaultRegion: 'ASD',
+        awsSecretKey: 'ASD'
+      }
+      const payload = getMockAuthorizedEvent(secretsPayload, {
+        id: createdFork.id
       })
-      it('Should be able to set secrets for fork', async () => {
-        const createdUser = await createMockUser()
-        const createdFork = await createMockFork(createdUser.githubId)
-        const secretsPayload: ForkAwsSecretArgs = {
-          awsAccessKey: 'ASD',
-          awsDefaultRegion: 'ASD',
-          awsSecretKey: 'ASD'
+      mockedAxios.get.mockResolvedValue({
+        data: {
+          ...getMockGithubUserFields(),
+          // These are real public keys, they are ok to leak
+          key: 'iXTwtNVkfNENdbMINv0Iqn90jgtJBEk0Hs7bILQ7S0k=',
+          key_id: '568250167242549743'
         }
-        const payload = getMockAuthorizedEvent(secretsPayload, {
-          id: createdFork.id
-        })
-        mockedAxios.get.mockResolvedValue({
-          data: {
-            ...getMockGithubUserFields(),
-            // These are real public keys, they are ok to leak
-            key: 'iXTwtNVkfNENdbMINv0Iqn90jgtJBEk0Hs7bILQ7S0k=',
-            key_id: '568250167242549743'
-          }
-        })
-        mockedAxios.put.mockResolvedValue({})
-        const result = await putSecretsHandler(payload)
-        expect(result.statusCode).toEqual(HttpStatus.NO_CONTENT)
-
-        const forkInDb = await prisma.fork.findFirst({
-          where: {
-            id: createdFork.id
-          }
-        })
-        expect(forkInDb?.secretsSet).toEqual(true)
       })
+      mockedAxios.put.mockResolvedValue({})
+      const result = await putSecretsHandler(payload)
+      expect(result.statusCode).toEqual(HttpStatus.NO_CONTENT)
+
+      const forkInDb = await prisma.fork.findFirst({
+        where: {
+          id: createdFork.id
+        }
+      })
+      expect(forkInDb?.secretsSet).toEqual(true)
     })
   })
 })
